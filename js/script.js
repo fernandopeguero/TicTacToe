bedb010f20d37b4a7e93a4e914275d51c4146222
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-13 16:02:35 -0500

    Add logic to create game board and cell

--- js/script.js
+++ js/script.js
@@ -2,17 +2,102 @@
 
 
 function GameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
+
+    /* Create board grid */
+    for(let i = 0; i < rows; i++){
+        board[i] = []
+        for(let j =0; j < columns; j++){
+            board[i].push(Cell())    
+        }
+    }
+
+    const getBoard = () => board;
+
+    /* Print  */
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+      };
+
+
+    const placeToken = (position, token) => {
+        board[position.row, position.column].addToken(token)
+
+    }
+
+      return {
+        getBoard,
+        printBoard,
+        placeToken
+      }
 }
 
 
 function Cell() {
 
+    let value = 0;
+
+    const addToken = (player ) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue,
+        placeToken
+    }
+
 }
 
 
-function GameController(){
+function GameController(
+    playerOne = "Player One",
+    playerTwo = "Player Two"
+){
 
+    const board = GameBoard();
+
+    const players =  [
+        {
+            name: playerOne,
+            token: "X"
+        } , 
+        {
+            name: playerTwo,
+            token: "O"
+        }
+    ]
+
+
+    let activePlayer = players[0]
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+    }
+
+    const getActivePlayer = () => activePlayer
+
+    const printNewRound = () => {
+        board.getBoard()
+        console.log(`${activePlayer.name}'s turn`);
+    }
+
+    const playRound = (position) => {
+
+    }
+
+
+
+    return {
+        getActivePlayer,
+        
+    }
 }
 
 
@@ -23,3 +108,6 @@ function ScreenController () {
 function ClickHandler() {
 
 }
+
+
+const game = GameBoard()
